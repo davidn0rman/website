cfd3524f072a5795b23ce494bcffb8b7d53c7fd3
David Norman <rockbigdave@gmail.com>
2019-05-30 20:25:04 +0100

    Removed a naughty word

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="Suck mah dick" />
+    <Header headerText="Header 1" />
     <Header headerText="It's pretty cool" /> 
 
     <p>Such wow. Very React.</p>
