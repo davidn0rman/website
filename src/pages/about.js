74b35480b67c21037b53e54e39b9f433e964af30
David Norman <rockbigdave@gmail.com>
2020-02-01 14:22:49 +0000

    Fixed deploy issue

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,7 @@ import Header from "../components/header/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <!--<Header headerText="Header 1" />-->
     <Header headerText="About me" /> 
-
     <p>Such wow. Very React.</p>
   </div>
 )
