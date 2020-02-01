05e9cc0de042e635cbf57196bfa721e3a7c4aa78
David Norman <rockbigdave@gmail.com>
2020-02-01 14:19:40 +0000

    Updated about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,8 @@ import Header from "../components/header/header"
 
 export default () => (
   <div style={{ color: `teal` }}>
-    <Header headerText="Header 1" />
-    <Header headerText="It's pretty cool" /> 
+    <!--<Header headerText="Header 1" />-->
+    <Header headerText="About me" /> 
 
     <p>Such wow. Very React.</p>
   </div>
