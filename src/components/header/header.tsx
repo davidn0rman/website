1d01f7edb4e9a3d1b0084c20a1670375c69c3003
David Norman <rockbigdave@gmail.com>
2020-08-19 08:30:45 +0100

    Various fixes for linting

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Header = ({ headerText }): JSX.Element => (
-  <h1>{headerText}</h1>
-);
+const Header = ({ headerText }): JSX.Element => <h1>{headerText}</h1>;
 
 Header.propTypes = {
   headerText: PropTypes.string.isRequired,
--- src/components/shapes/cross.tsx
+++ src/components/shapes/cross.tsx
@@ -12,41 +12,30 @@ interface ICrossProps {
   top: string;
   size: string;
   rotate: string;
-};
+}
 
 const Cross: React.FC<ICrossProps> = (props: ICrossProps): JSX.Element => {
-  const {
-    bottom,
-    colour,
-    opacity,
-    left,
-    right,
-    top,
-    size,
-    rotate,
-  } = props;
+  const { bottom, colour, opacity, left, right, top, size, rotate } = props;
 
   const classes = classNames(
     styles.cross,
     colour === "purple" ? `${styles.purple}` : "",
-    colour === "blue" ? `${styles.blue}` : "",
+    colour === "blue" ? `${styles.blue}` : ""
   );
 
   return (
     <div
       className={classes}
-      style={
-        {
-          bottom: `${bottom}%`,
-          height: `${size}rem`,
-          left: `${left}%`,
-          opacity,
-          right: `${right}%`,
-          top: `${top}%`,
-          transform: `rotate(${rotate}deg)`,
-          width: `${size}rem`,
-        }
-      }
+      style={{
+        bottom: `${bottom}%`,
+        height: `${size}rem`,
+        left: `${left}%`,
+        opacity,
+        right: `${right}%`,
+        top: `${top}%`,
+        transform: `rotate(${rotate}deg)`,
+        width: `${size}rem`,
+      }}
     />
   );
 };
